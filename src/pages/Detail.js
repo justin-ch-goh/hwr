1edd422abbbd304b2d537b9213389146ab5ac432
justin-ch-goh <justin.ch.goh@gmail.com>
2016-03-11 01:15:38 +0800

    first commit

---
+++ src/pages/Detail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Chance from 'chance';
+
+class Detail extends React.Component {
+    render() {
+        return (<div><p>Hello, {chance.first()}!</p>
+            <p>You're from {chance.country({ full: true})}.</p>
+        </div>);
+    }
+}
+
+export default Detail;
