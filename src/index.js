4cae8b9425c845d9e148be2621e32d1141d95660
justin-ch-goh <justin.ch.goh@gmail.com>
2016-03-11 15:48:58 +0800

    routes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
 import { createHashHistory } from 'history'; 
 
-import Detail from './pages/Detail';
-import List from './pages/List';
+import routes from './routes';
 
 const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 // https://github.com/reactjs/react-router/blob/master/docs/API.md#example-3
@@ -13,8 +12,7 @@ const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 ReactDOM.render(
     <Router history={appHistory}
         onUpdate={() => window.scrollTo(0, 0)}>
-        <Route path="/" component={ List } />
-        <Route path="/detail/:repo" component={ Detail } />
+        {routes}
     </Router>,
     document.getElementById('app')
 );
