b821a3b8d5bc90dcbfb7a9a440907bec974efb5f
justin-ch-goh <justin.ch.goh@gmail.com>
2016-03-11 09:41:43 +0800

    git api

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import Chance from 'chance';
+import ajax from 'superagent';
 
 class Detail extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { commits: [] };
+    }
+
+    componentWillMount() {
+        ajax.get('https://api.github.com/repos/facebook/react/commits')
+            .end((error, response) => {
+                if (!error && response) {
+                    console.dir(response.body);
+                    this.setState({ commits: response.body })
+                } else {
+                    console.log('Error fetching from GitHub');
+                }
+            }
+        );
+
+    }
+
+
     render() {
-        return (<div><p>Hello, {chance.first()}!</p>
-            <p>You're from {chance.country({ full: true})}.</p>
-        </div>);
+        return (<div>
+            {this.state.commits.map((commit, index) => {
+                const author = commit.author ? commit.author.login : 'Anonymous';
+
+                (<p key={index}>
+                    <strong>{author}: </strong>
+                    <a href={commit.html_url}>{commit.commit.message}</a>
+                </p>)
+            })}
+        </div>); //binding this as in Detail rather than button so that this.buttonClicked makes more sense than button.buttonClicked
     }
 }
 
