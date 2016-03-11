8ec1b1ee260ac3157a3ed2cffe42200f4bbe4320
justin-ch-goh <justin.ch.goh@gmail.com>
2016-03-11 12:43:17 +0800

    custom URLs with router params

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history'; 
 
 import Detail from './pages/Detail';
+import List from './pages/List';
+
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
+// https://github.com/reactjs/react-router/blob/master/docs/API.md#example-3
+
 
 ReactDOM.render(
-    <Detail />,
+    <Router history={appHistory}
+        onUpdate={() => window.scrollTo(0, 0)}>
+        <Route path="/" component={ List } />
+        <Route path="/detail/:repo" component={ Detail } />
+    </Router>,
     document.getElementById('app')
 );
